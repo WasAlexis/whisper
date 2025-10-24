4bbb243114c7e1e8fb5df2fed5ebe3d4db68c97a
Alexis <alexisnomain@icloud.com>
2025-10-24 10:21:17 -0400

    feat: add handle message

--- server/index.js
+++ server/index.js
@@ -1,8 +1,27 @@
+import http from 'node:http';
 import express from 'express';
+import { WebSocketServer } from 'ws';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const server = http.createServer(app);
+
+const wss = WebSocketServer({ server });
+
+wss.on('connection', (ws) => {
+  console.log('New client connected');
+
+  ws.on('message', (message) => {
+    console.log(`Received message: ${message}`);
+    ws.send(`Echo: ${message}`);
+  });
+
+  ws.on('close', () => {
+    console.log('Client disconnected');
+  });
+})
+;
 app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
