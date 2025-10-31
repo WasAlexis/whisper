2d6ecf45639e889c0eff8e99646f7b5555484956
Alexis <alexisnomain@icloud.com>
2025-10-31 13:25:39 -0400

    feat: basic logic to send message

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,5 +1,6 @@
 import './styles.css';
 import { useState } from 'preact/hooks';
+import { useEffect } from 'preact/hooks';
 import { useRef } from 'preact/hooks';
 import Message from './components/Message.jsx';
 
@@ -8,6 +9,31 @@ function App() {
   const [messages, setMessages] = useState([]);
   const textareaRef = useRef(null);
   const chatHistoryRef = useRef(null);
+  const wsRef = useRef(null);
+
+  useEffect(() => {
+    if (username) {
+      const ws = new WebSocket('ws://192.168.1.62:3000');
+
+      ws.onopen = () => {
+        console.log('Connected to WebSocket server');
+        wsRef.current = ws;
+      }
+
+      ws.onmessage = (event) => {
+        const message = event.data;
+        setMessages(prevMessages => [...prevMessages, JSON.parse(message)]);
+      }
+
+      ws.onclose = () => {
+        console.log('Disconnected from WebSocket server');
+      }
+
+      return () => {
+        ws.close();
+      };
+    }
+  }, [username]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -19,11 +45,14 @@ function App() {
       id: Date.now(),
       username: username,
       text: text.trim(),
-      language: 'en',
+      language: localStorage.getItem('language') || 'EN',
       timestamp: new Date().toLocaleTimeString()
     };
 
-    setMessages([...messages, newMessage]);
+    if (wsRef.current && wsRef.current.readyState === WebSocket.OPEN) {
+      wsRef.current.send(JSON.stringify(newMessage));
+    }
+
     textareaRef.current.value = '';
 
     setTimeout(() => {
@@ -41,6 +70,7 @@ function App() {
             const username = document.getElementById('username').value;
             if (username.trim() !== '') {
               localStorage.setItem('username', username.trim());
+              setUsername(username.trim());
               window.location.reload();
             }
           }}>Get in</button>
@@ -53,12 +83,14 @@ function App() {
     <div className='chat-container'>
       <div className="chat-history" ref={chatHistoryRef}>
         {messages.map(msg => (
-          <Message key={msg.id} text={msg.text} sender={msg.username} />
+          <Message key={msg.id} text={msg.text} 
+          sender={msg.username} 
+          translatedText={msg.translatedText} />
         ))}
       </div>
       <form className="chat-input" onSubmit={handleSubmit}>
         <textarea placeholder="Type your message..." ref={textareaRef} />
-        <button type='submit'>Send</button>
+        <button type='submit'></button>
       </form>
     </div>
   );
--- server/index.js
+++ server/index.js
@@ -7,25 +7,58 @@ const PORT = process.env.PORT || 3000;
 
 const server = http.createServer(app);
 
-const wss = WebSocketServer({ server });
+const wss = new WebSocketServer({ server });
+
+function broadcastMessage(data) {
+  wss.clients.forEach(async (client) => {
+    if (client.readyState === client.OPEN) {
+      client.send(JSON.stringify(
+        {
+          id: Date.now(),
+          username: data.username,
+          text: data.text,
+          translatedText: await translateText(data.text, data.language),
+          language: data.language,
+          timestamp: new Date().toLocaleTimeString()
+        }
+      ));
+    }
+  });
+}
+
+async function translateText(text, language) {
+  const targetLang = (language === 'ES') ? 'EN' : 'ES';
+  const response = await fetch('https://api-free.deepl.com/v2/translate', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Authorization': `DeepL-Auth-Key ${process.env.DEEPL_API_KEY}`
+    },
+    body: new URLSearchParams({
+      text: text,
+      target_lang: targetLang
+    })
+  });
+  const data = await response.json();
+  return data.translations[0].text;
+}
 
 wss.on('connection', (ws) => {
   console.log('New client connected');
 
   ws.on('message', (message) => {
-    console.log(`Received message: ${message}`);
-    ws.send(`Echo: ${message}`);
+    broadcastMessage(JSON.parse(message));
   });
 
   ws.on('close', () => {
     console.log('Client disconnected');
   });
-})
-;
+});
+
 app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
