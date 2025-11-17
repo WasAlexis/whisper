632d7d48c4bc21193bc3b21283131439ae08df3c
Alexis <alexisnomain@icloud.com>
2025-11-16 23:07:25 -0400

    feat: create chat page

--- client/src/pages/Chat.jsx
+++ client/src/pages/Chat.jsx
@@ -1,6 +1,17 @@
+import '../styles/chat.css';
+import Message from '../component/Message/Message.jsx';
+
 function Chat() {
     return (
-        <h1>Chat</h1>
+        <div className="chat-container">
+            <div className="chat-history">
+                <h2>Whisper</h2>
+                <Message text="Hello, how are you?" sender="Alice" translated="Hola, ¿cómo estás?" />
+            </div>
+            <div className="chat-input">
+                <textarea placeholder="Type your message..." />
+            </div>
+        </div>
     );
 }
 
